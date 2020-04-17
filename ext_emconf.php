3aa09b27de3f542b3135c93f02e124ff64a449b2
Michiel Roos <michiel@michielroos.com>
2020-04-17 19:38:27 +0200

    Prepare for release of v1.2.4 on packagist (#5)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,25 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Open Graph',
     'description' => 'Manage open graph tags in TYPO3 inside Page Properties',
     'category' => 'plugin',
-    'version' => '1.2.3',
+    'version' => '1.2.4',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'author' => 'Jesper Paardekooper',
     'author_email' => 'j.paardekooper@develement.nl',
     'author_company' => 'DevElement',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '6.2.0-8.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    'autoload' => array(
-        'psr-4' => array(
-            'Keizer\\KoningOpenGraph\\' => 'Classes'
-        )
-    ),
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+
+    'autoload' => [
+        'psr-4' => [
+            'Keizer\\KoningOpenGraph\\' => 'Classes',
+        ],
+    ],
+];
